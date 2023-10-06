0d150a801c17b880a7486bdd42ab55216b76a3ac
Anirudha <anirudhapsah@gmail.com>
2023-10-06 13:56:32 +0530

    uploadthing v5.6

--- app/(Site)/layout.jsx
+++ app/(Site)/layout.jsx
@@ -1,31 +1,34 @@
-'use client'
+"use client";
 
 import { AuthContextProvider } from "@/context/authContext";
 import Navigation from "@/components/navigation";
 import Loading from "./loading";
 import { Suspense } from "react";
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { extractRouterConfig } from "uploadthing/server";
+import { ourFileRouter } from "../api/uploadthing/core";
 
-
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 export default function Layout({ children }) {
   return (
-      <body className="bg-[#000]">
+    <body className="bg-[#000]">
+      <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
       <QueryClientProvider client={queryClient}>
         <AuthContextProvider>
-            <Navigation />
-            <main className="w-full md:w-[70%] max-w-4xl m-auto md:ml-52 lg:ml-60 h-full">
-              <Suspense fallback={<Loading />}>
-                {children}
-                <Analytics />
-              </Suspense>
-            </main>
+          <Navigation />
+          <main className="w-full md:w-[70%] max-w-4xl m-auto md:ml-52 lg:ml-60 h-full">
+            <Suspense fallback={<Loading />}>
+              {children}
+              <Analytics />
+            </Suspense>
+          </main>
         </AuthContextProvider>
         <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
-      </body>
+      </QueryClientProvider>
+    </body>
   );
 }
