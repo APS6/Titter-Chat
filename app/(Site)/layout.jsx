1d35d05fcfd9dbd686fb9387ab5199a0f78c38a6
Anirudha <anirudhapsah@gmail.com>
2023-11-29 13:24:50 +0530

    enable notifications toast

--- app/(Site)/layout.jsx
+++ app/(Site)/layout.jsx
@@ -13,7 +13,7 @@ import { Toaster, toast } from "sonner";
 import { ContextProvider } from "@/context/context";
 import { initFirebase } from "@/firebase/app";
 import { getAuth } from "firebase/auth";
-import { useRouter } from "next/navigation";
+import { redirect, usePathname } from "next/navigation";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { getMessaging, onMessage, isSupported } from "firebase/messaging";
 const queryClient = new QueryClient();
@@ -21,10 +21,10 @@ const queryClient = new QueryClient();
 export default function Layout({ children }) {
   const auth = getAuth(initFirebase());
   const [user, loading, error] = useAuthState(auth);
-  const router = useRouter();
+  const pathname = usePathname()
 
   if (!user && !loading && !error) {
-    router.push("/SignIn");
+    redirect("/SignIn");
   }
 
   useEffect(() => {
@@ -42,6 +42,20 @@ export default function Layout({ children }) {
         };
 
         if (messagingSupported()) {
+          if (pathname !== "/settings" && Notification.permission === 'default') {
+            toast("Don't miss a thing! Enable notifications for instant updates.", {
+              duration: 10000,
+              icon: <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M5 19q-.425 0-.713-.288T4 18q0-.425.288-.713T5 17h1v-7q0-2.075 1.25-3.688T10.5 4.2v-.7q0-.625.438-1.063T12 2q.625 0 1.063.438T13.5 3.5v.7q2 .5 3.25 2.113T18 10v7h1q.425 0 .713.288T20 18q0 .425-.288.713T19 19H5Zm7-7.5ZM12 22q-.825 0-1.413-.588T10 20h4q0 .825-.588 1.413T12 22Zm-4-5h8v-7q0-1.65-1.175-2.825T12 6q-1.65 0-2.825 1.175T8 10v7Z"></path></svg>,
+              action: {
+                label: "Enable now!",
+                onClick: () => {redirect('/settings')}
+              },
+              cancel: {
+                label: "Not now",
+              }
+            })
+          }
+
           messaging = getMessaging();
 
           onMessage(messaging, (payload) => {
