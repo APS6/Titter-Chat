1b388688c7e8d3d540956a25dbfdfea72fab4db4
Anirudha <anirudhapsah@gmail.com>
2023-12-08 19:46:32 +0530

    fixed logic

--- app/api/chatUser/[username]/[token]/route.js
+++ app/api/chatUser/[username]/[token]/route.js
@@ -2,42 +2,35 @@ import { prisma } from "@/app/lib/db";
 import { NextResponse } from "next/server";
 import admin from "@/app/lib/firebaseAdmin"
 
-export async function GET(req, {params}) {
+export async function GET(req, { params }) {
     const { username, token } = params;
     try {
         const decodedToken = await admin.auth().verifyIdToken(token);
         const userId = decodedToken.uid;
         if (!userId) {
             return NextResponse.json({ error: 'Failed Authorization', success: false }, { status: 400 });
-        } else if (userId){
-        const user = await prisma.user.findFirst({
-            select: {
-                username: true,
-                pfpURL: true,
-                id: true,
-                following: {
-                    where: {
-                        followingId: userId
-                    },
+        } else if (userId) {
+            const user = await prisma.user.findFirst({
                 select: {
+                    username: true,
+                    pfpURL: true,
+                    id: true,
+                    allowDMs: true,
                     following: {
-                        select: {
-                            username: true,
-                            allowDMs: true,
-                        }
+                        where: {
+                            followingId: userId
+                        },
                     }
+                },
+                where: {
+                    username: username
                 }
-                }
-            },
-            where: {
-                username: username
+            });
+            if (user.following.length !== 0 || user.allowDMs) {
+                return NextResponse.json({ user: user, allowMessage: true }, { status: 200 });
             }
-        });
-        if (user.following.length === 0 || !user.following[0]?.following?.allowDMs ) {
-            return NextResponse.json({user: user, allowMessage: false, currentUser: user.following[0].following.username}, { status: 200 });
+            return NextResponse.json({ user: user, allowMessage: false }, { status: 200 });
         }
-        return NextResponse.json({user: user, allowMessage: true, currentUser: user.following[0].following.username }, { status: 200 });
-    }
     } catch (error) {
         console.error('Error retrieving user data', error);
         return NextResponse.json({ error: 'Error retrieving user data', success: false }, { status: 500 });
