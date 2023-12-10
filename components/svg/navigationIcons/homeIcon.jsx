b667926b6d11f93190bec7efecb9bd8be38354b0
Anirudha <anirudhapsah@gmail.com>
2023-12-10 16:52:44 +0530

    fixed svg

--- components/svg/navigationIcons/homeIcon.jsx
+++ components/svg/navigationIcons/homeIcon.jsx
@@ -16,11 +16,16 @@ export default function HomeIcon({ active }) {
     );
   } else {
     return (
-      <svg fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+      >
         <path
-          d="M19.5 9.25V20.5h-5v-7h-5v7h-5V9.25L12 3.625l7.5 5.625z"
-          stroke="#fff"
-        />
+          fill="white"
+          d="M6 19h3v-6h6v6h3v-9l-6-4.5L6 10zm-2 2V9l8-6l8 6v12h-7v-6h-2v6zm8-8.75"
+        ></path>
       </svg>
     );
   }
