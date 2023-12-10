2f0a87e771d2ded348fd52b0d16efc120b3117db
Anirudha <anirudhapsah@gmail.com>
2023-12-10 17:49:26 +0530

    padding on links

--- components/mobileNavigation.jsx
+++ components/mobileNavigation.jsx
@@ -31,35 +31,34 @@ export default function MobileNavigation() {
   }
 
   return (
-    <div className="md:hidden bg-[#000] border-t border-t-lightwht flex justify-between fixed bottom-0 w-full py-3 px-4 z-10">
-      <Link href="/Home">
+    <div className="md:hidden bg-[#000] border-t border-t-lightwht flex justify-between fixed bottom-0 w-full px-4 z-10">
+      <Link href="/Home" className="p-3">
         <HomeIcon active={pathname === "/Home"} />
       </Link>
-      <Link href="/DMs">
+      <Link href="/DMs" className="p-3">
         <MessageIcon active={pathname.startsWith("/DMs")} />
       </Link>
-      <Link href="/settings">
+      <Link href="/settings" className="p-3">
         <SettingsIcon active={pathname === "/settings"} />
       </Link>
-      <Link
-        href={`/profile/${_user?.username}`}
-        className=" bg-grey rounded-full w-6 h-6"
-      >
-        {_user ? (
-          <Image
-            width={24}
-            height={24}
-            src={_user?.pfpURL}
-            alt="You"
-            className={`rounded-full border-white ${
-              pathname === "/profile/" + _user?.username
-                ? "border-1"
-                : "border-0"
-            }`}
-          />
-        ) : (
-          ""
-        )}
+      <Link href={`/profile/${_user?.username}`} className="p-3">
+        <div className="rounded-full h-6 w-6 bg-grey">
+          {_user ? (
+            <Image
+              width={24}
+              height={24}
+              src={_user?.pfpURL}
+              alt="You"
+              className={`rounded-full border-white ${
+                pathname === "/profile/" + _user?.username
+                  ? "border-1"
+                  : "border-0"
+              }`}
+            />
+          ) : (
+            ""
+          )}
+        </div>
       </Link>
     </div>
   );
