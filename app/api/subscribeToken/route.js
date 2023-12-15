8296624354479adb0c5a0e76dedc707d543e0011
Anirudha <anirudhapsah@gmail.com>
2023-12-15 18:13:20 +0530

    one last try

--- app/api/subscribeToken/route.js
+++ app/api/subscribeToken/route.js
@@ -6,20 +6,17 @@ import { headers } from "next/headers";
 
 export async function POST(req) {
     const body = await req.json()
-    const headersList = headers();
-    const accessToken = headersList.get("authorization");
     try {
-        const decodedToken = await admin.auth().verifyIdToken(accessToken);
-        const userId = decodedToken.uid;
-        if (!userId) {
-            return NextResponse.json({ error: 'Failed Authorization', success: false }, { status: 400 });
-        }
-        admin.messaging().subscribeToTopic(body.token, userId).then(() => {
-            return NextResponse.json({ success: true }, { status: 200 });
+        const subscribe = await admin.messaging().subscribeToTopic(body.token, userId).then(() => {
+            return true
         }).catch((error) => {
             console.error('Error subscribing user to topic:', error);
-            return NextResponse.json({ error: 'Error subscribing to topic', success: false }, { status: 500 });
+            return false
         });
+        if (!subscribe) {
+            return NextResponse.json({ error: 'Error subscribing to topic', success: false }, { status: 500 });
+        }
+        return NextResponse.json({ success: true }, { status: 200 });
     } catch (error) {
         console.error('Request error', error);
         return NextResponse.json({ error: 'Error Saving token', success: false }, { status: 500 });
