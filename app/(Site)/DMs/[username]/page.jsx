9f357f3a6f129cc59baaab935f95077c7035e32e
Anirudha <anirudhapsah@gmail.com>
2023-12-16 18:03:35 +0530

    styling if there arent enough messages

--- app/(Site)/DMs/[username]/page.jsx
+++ app/(Site)/DMs/[username]/page.jsx
@@ -101,9 +101,9 @@ export default function DMUser({ params }) {
   const messages = data?.pages?.flatMap((page) => page.items);
 
   return (
-    <div className="full-height flex flex-col px-1">
-      <Link className="ml-2 mt-1" href={`/profile/${username}`}>
-        <div className="flex gap-2 items-center">
+    <div className="full-height px-1 pt-1">
+      <Link href={`/profile/${username}`}>
+        <div className="flex gap-2 pl-2 items-center">
           {chatUser?.data?.user?.pfpURL ? (
             <Image
               src={chatUser?.data?.user?.pfpURL}
@@ -120,61 +120,63 @@ export default function DMUser({ params }) {
           </h2>
         </div>
       </Link>
-      <ScrollToBottom
-        className="relative mt-1 overflow-y-auto"
-        followButtonClassName="hidden"
-        scrollViewClassName="pt-1"
-      >
-        {status !== "loading" ? (
-          <div className="flex flex-col-reverse gap-2">
-            {messages?.map((message, i) => {
-              if (window.innerWidth > 768) {
-                return (
-                  <DMContextMenu
-                    key={message.id}
-                    cUsername={username}
-                    divRef={i === messages.length - 1 ? lastDivRef : null}
-                    message={message}
-                    setReplying={setReplying}
-                    setReplyingTo={setReplyingTo}
-                  />
-                );
-              } else
-                return (
-                  <DMMessage
-                    key={message.id}
-                    cUsername={username}
-                    divRef={i === messages.length - 1 ? lastDivRef : null}
-                    message={message}
-                    setReplying={setReplying}
-                    setReplyingTo={setReplyingTo}
-                  />
-                );
-            })}
-            <ScrollDown />
-            {isFetchingNextPage ? (
-              <div className="py-2 w-full grid place-items-center">
-                <Loader />
-              </div>
-            ) : (
-              ""
-            )}
-          </div>
-        ) : (
-          <div className="h-[100svh] w-full grid place-items-center">
-            <BlockLoader />
-          </div>
-        )}
-      </ScrollToBottom>
-      <DMInput
-        sendingTo={chatUser?.data?.user?.id}
-        disabled={chatUser && !chatUser?.data?.allowMessage}
-        replying={replying}
-        replyingTo={replyingTo}
-        setReplying={setReplying}
-        setReplyingTo={setReplyingTo}
-        cUsername={username}
-      />
+      <div className="flex flex-col h-[calc(100svh-44px)] justify-between">
+        <ScrollToBottom
+          className="relative mt-1 overflow-y-auto"
+          followButtonClassName="hidden"
+          scrollViewClassName="pt-1"
+        >
+          {status !== "loading" ? (
+            <div className="flex flex-col-reverse gap-2">
+              {messages?.map((message, i) => {
+                if (window.innerWidth > 768) {
+                  return (
+                    <DMContextMenu
+                      key={message.id}
+                      cUsername={username}
+                      divRef={i === messages.length - 1 ? lastDivRef : null}
+                      message={message}
+                      setReplying={setReplying}
+                      setReplyingTo={setReplyingTo}
+                    />
+                  );
+                } else
+                  return (
+                    <DMMessage
+                      key={message.id}
+                      cUsername={username}
+                      divRef={i === messages.length - 1 ? lastDivRef : null}
+                      message={message}
+                      setReplying={setReplying}
+                      setReplyingTo={setReplyingTo}
+                    />
+                  );
+              })}
+              <ScrollDown />
+              {isFetchingNextPage ? (
+                <div className="py-2 w-full grid place-items-center">
+                  <Loader />
+                </div>
+              ) : (
+                ""
+              )}
+            </div>
+          ) : (
+            <div className="h-[100svh] w-full grid place-items-center">
+              <BlockLoader />
+            </div>
+          )}
+        </ScrollToBottom>
+        <DMInput
+          sendingTo={chatUser?.data?.user?.id}
+          disabled={chatUser && !chatUser?.data?.allowMessage}
+          replying={replying}
+          replyingTo={replyingTo}
+          setReplying={setReplying}
+          setReplyingTo={setReplyingTo}
+          cUsername={username}
+        />
+      </div>
     </div>
   );
 }
